cc2c1f5c127c8f72ca2ca7ceda5ec2fe02a83330
JaedenT <95214533+Jaeden-T@users.noreply.github.com>
2022-11-23 21:52:36 +0800

    Update 20Mark June 2020.py
    Add comments and fix bracket

--- 20Mark June 2020.py
+++ 20Mark June 2020.py
@@ -50,34 +50,42 @@ def guessCapital():
     # Add your code here
 
 
-
+     #A while loop to make sure the code runs until 5 questions filled in
     while questionCount != 6:
-        avalibleQuestions = []
+          #Creates a array to store the vaalible questions
+        avalibleQuestions = [] 
         questionSelect = -1
-
+          
+          #Adds the valid questions to the avalible questions array to later print to theu ser
         for x in questions:
             if x != 0:
                 avalibleQuestions.append(x)
-
+          
+          #Print out the questions
         print("The questions area: ", avalibleQuestions)
-        print("\n"
-
+        print("\n")
+          
+          #Validation uses to make sure user selects a valid one
         while questionSelect not in avalibleQuestions:
             questionSelect = int(input("Select a question: "))
-
+          #Print question and accept input
         print("What is the capital of " + countries[questionSelect - 1] + "?")
         answer = input(": ")
 
+               # .upper validation to prevent London/london/LONdon from not being accepted
         if answer.upper() == capitals[questionSelect - 1].upper():
             print("That is correct! ")
             questionScore += 1
         else:
             print("That is wrong the capital of " + countries[questionSelect - 1] + " is " + capitals[questionSelect - 1]  )
-
+          
+          #New line formating
         print("\n")
+         #Set question to 0 so it WILL never be reused 
+          #More effcient then removing array elemnts as it can get messy on long lists
         questions[questionSelect - 1] = 0
 
-
+          
         questionCount += 1
 
 
@@ -96,6 +104,7 @@ while menuChoice != 3:
     # Add your code to:
     #   call the relevant subprogram if the menu choice is 1 or 2
     #   display the player name and the score if the menu choice is 3
+
     if menuChoice == 1:
         playerName = addPlayerName(playerName)
     elif menuChoice == 2:
